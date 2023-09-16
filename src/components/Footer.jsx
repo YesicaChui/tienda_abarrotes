5366b8285e90b5ec575739e565ff79ec3315da4f
YesicaChui <yesicachuic@gmail.com>
2023-09-15 21:21:32 -0500

    responsive para productos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <p>
         <a target="_blank" href="https://www.linkedin.com/in/yesicachuichalco/" >Yesica Chui Chalco</a>
         © 2023 - <strong> Todos los derechos reservados </strong></p>
-      <p > Disclaimer: Esta página es un proyecto educativo. El cual me ayudo a reforzar mis habilidades </p>
+      <p > Esta página es un proyecto educativo. Este proyecto ayudo a reforzar mis habilidades como desarrolladora Web </p>
     </footer>
   )
 }
