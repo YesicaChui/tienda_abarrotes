130e3edae983b7933db5c26fe98ef1128f9606e2
YesicaChui <yesicachuic@gmail.com>
2023-09-15 22:48:58 -0500

    estilizando el carrito

--- src/components/CartViewItem.jsx
+++ src/components/CartViewItem.jsx
@@ -2,25 +2,25 @@ import React, { useContext } from 'react'
 import { ItemCount } from './ItemCount'
 import { CartContext } from '../context/CartContext'
 
-export const CartViewItem = ({ producto,index, borrarDelCarrito }) => {
+export const CartViewItem = ({ producto, index, borrarDelCarrito }) => {
 
-  const {updateCart} = useContext(CartContext)
+  const { updateCart } = useContext(CartContext)
 
-  const setCantidad = (cantidad)=>{
+  const setCantidad = (cantidad) => {
     updateCart(producto.id, cantidad)
   }
 
   return (
     <tr className='fila' key={producto.id} >
-    <td>{index + 1}</td>
-    <td> <img src={producto.img} alt={producto.nombre} className='fila__img' /></td>
-    <td>{producto.nombre}</td>
-    <td> <ItemCount cantidad={producto.cantidad} stock={producto.stock} setCantidad={setCantidad}/></td>
-    <td>S/. {producto.precio}</td>
-    <td>S/. {(producto.cantidad * producto.precio).toFixed(2)}</td>
-    <td>
-      <button className='btnDelete' onClick={()=>borrarDelCarrito(producto.id)}>🗑️</button>
-    </td>
-  </tr>
+      <td>{index + 1}</td>
+      <td> <img src={producto.img} alt={producto.nombre} className='fila__img' /></td>
+      <td>{producto.nombre}</td>
+      <td> <ItemCount cantidad={producto.cantidad} stock={producto.stock} setCantidad={setCantidad} /></td>
+      <td>S/. {producto.precio}</td>
+      <td>S/. {(producto.cantidad * producto.precio).toFixed(2)}</td>
+      <td>
+        <button className='btnDelete' onClick={() => borrarDelCarrito(producto.id)}>🗑️</button>
+      </td>
+    </tr>
   )
 }
