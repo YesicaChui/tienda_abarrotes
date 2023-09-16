bc8c9289471f2f11bc049d5837866fea1aeabdfb
YesicaChui <yesicachuic@gmail.com>
2023-09-15 22:02:11 -0500

    agregando responsive con menu sandwich

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,24 +2,35 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/logo_vilma.png'
 
 import { CartWidget } from './CartWidget'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { CartContext } from '../context/CartContext'
 export const NavBar = () => {
   const { cart } = useContext(CartContext)
+  const [menuVisible, setMenuVisible] = useState(false);
   const handleCartClick = () => {
       alert("No hay productos en el carrito");
   };
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <header className='header'>
       <section className='header__container'>
         <Link to={"/"}> <img src={logo} alt="logo" className='header__logo' /></Link>
       </section>
-      <nav className='navbar'>
+      <nav className={`navbar ${menuVisible ? 'active' : ''}`}>
         <Link to="/category/alimentos" className='navbar__link'>Alimentos</Link>
         <Link to="/category/bebidas" className='navbar__link'>Bebidas</Link>
         <Link to="/category/confiteria" className='navbar__link'>Confiteria</Link>
         <Link to="/category/limpieza" className='navbar__link'>Productos de Limpieza</Link>
       </nav>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
       {cart.length !== 0
         ? (
           <Link to={"/cart"}>
